9a2b1cf5197d982bae84553f24cb7ed23fcd096a
DilandLp <dilandlopez10@gmail.com>
2024-09-07 15:51:43 -0500

    Logica de operadores lista

--- src/main/java/com/co/jdk_calculator/Controllers/ViewCalculatorController.java
+++ src/main/java/com/co/jdk_calculator/Controllers/ViewCalculatorController.java
@@ -1,5 +1,6 @@
 package com.co.jdk_calculator.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
@@ -33,6 +34,42 @@ public class ViewCalculatorController {
         if (currentText!=null && currentText.length() > 0){
             txtNumeros.setText(currentText.substring(0, currentText.length() - 1));
         }
+    }
+
+    public  void handleOperatorClick(ActionEvent event){
+        Button button = (Button) event.getSource();
+        String buttonId = button.getId();
+        String currentText = txtNumeros.getText();
+
+        if (!currentText.isEmpty() && isOperator(currentText.charAt(currentText.length() - 1))) {
+            return;
+        }
+
+        switch (buttonId) {
+            case "btSuma":
+                operator = " + ";
+                break;
+            case "btResta":
+                operator = " - ";
+                break;
+            case "btMultiply":
+                operator = " x ";
+                break;
+            case "btDivide":
+                operator = " / ";
+                break;
+            default:
+                operator = "";
+                break;
+        }
+
+        if (!operator.isEmpty()) {
+            txtNumeros.setText(currentText + operator);
+        }
+    }
+
+    private boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
     }
 }
 
