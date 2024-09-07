0add72b5c0ce8c58201a7b5583d6adbea4b41a64
jhonparrajp <jparrapayares@gmail.com>
2024-09-07 15:31:24 -0500

    boton clear y all clear ,numeros convirtiendose a int

--- src/main/java/com/co/jdk_calculator/Controllers/ViewCalculatorController.java
+++ src/main/java/com/co/jdk_calculator/Controllers/ViewCalculatorController.java
@@ -30,12 +30,30 @@ public class ViewCalculatorController {
     @FXML
     private Text txtNumeros;
 
-    public void onButtonNums(javafx.event.ActionEvent event){
-        Button clickedButton = (Button)event.getSource();
+    private int suma = 0;
+
+    @FXML
+    public void onButtonNums(javafx.event.ActionEvent event) {
+        Button clickedButton = (Button) event.getSource();
         txtNumeros.setText(txtNumeros.getText() + clickedButton.getText());
+        System.out.println(txtNumeros.getText());
+        try {
+            int numero = Integer.parseInt(txtNumeros.getText());
+            System.out.println("Número convertido: " + numero);
+        } catch (NumberFormatException e) {
+            System.out.println("Error");
+        }
     }
+    public void clearAllButton(javafx.event.ActionEvent event) {
+        txtNumeros.setText("");
 
-
+    }
+    public void clearButton(javafx.event.ActionEvent event) {
+        String currentText = txtNumeros.getText();
+        if (currentText!=null && currentText.length() > 0){
+            txtNumeros.setText(currentText.substring(0, currentText.length() - 1));
+        }
+    }
 
 
 }
