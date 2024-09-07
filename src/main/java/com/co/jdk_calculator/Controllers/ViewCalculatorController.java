12f14977424c093fc980c7d0a32cbd17b71803d6
DilandLp <dilandlopez10@gmail.com>
2024-09-07 15:34:06 -0500

    Iniciando logica de operadores

--- src/main/java/com/co/jdk_calculator/Controllers/ViewCalculatorController.java
+++ src/main/java/com/co/jdk_calculator/Controllers/ViewCalculatorController.java
@@ -1,41 +1,45 @@
 package com.co.jdk_calculator.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class ViewCalculatorController {
-
-
-    @FXML
-    private Button Bt_Zero;
-    @FXML
-    private Button Bt_One;
-    @FXML
-    private Button Bt_Two;
-    @FXML
-    private Button Bt_Three;
-    @FXML
-    private Button Bt_Four;
-    @FXML
-    private Button Bt_Five;
-    @FXML
-    private Button Bt_Six;
-    @FXML
-    private Button Bt_Seven;
-    @FXML
-    private Button Bt_Eigth;
-    @FXML
-    private Button Bt_Nine;
     @FXML
     private Text txtNumeros;
+    private String operator = "";
+    private double firstNumber = 0;
+    private boolean startNewNumber = true;
+
 
     public void onButtonNums(javafx.event.ActionEvent event){
         Button clickedButton = (Button)event.getSource();
         txtNumeros.setText(txtNumeros.getText() + clickedButton.getText());
     }
 
+    @FXML
+    private void handleOperatorClick(ActionEvent event){
+        Button button = (Button) event.getSource();
+        String buttonId = button.getId();
 
-
-
+        switch (buttonId) {
+            case "btSuma":
+                operator = "+";
+                txtNumeros.setText(txtNumeros.getText() + operator);
+                break;
+            case "btResta":
+                operator = "-";
+                txtNumeros.setText(txtNumeros.getText() + operator);
+                break;
+            case "btMultiply":
+                operator = "*";
+                txtNumeros.setText(txtNumeros.getText() + operator);
+                break;
+            case "btDivide":
+                operator = "/";
+                txtNumeros.setText(txtNumeros.getText() + operator);
+                break;
+        }
+    }
 }
